4fab36068e7dc31e4494234e1ca3f3cac90a18e1
harsha <harshavardhan53595@gmail.com>
2026-08-19 17:16:51 +0530

    filtering and sorting added

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ app.get("/test", (req, res) => {
 async function startServer() {
     try {
         await sequelize.authenticate();
-
         console.log("Database connected successfully");
 
         const PORT = process.env.PORT || 5000;
@@ -33,11 +32,10 @@ async function startServer() {
         app.listen(PORT, () => {
             console.log(`Server running on port ${PORT}`);
         });
-
-    } catch (error) {
+    } 
+    catch (error) {
         console.error("Unable to connect to database:");
         console.error(error);
     }
 }
-
 startServer();
--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,4 +1,5 @@
 import { Task } from "../models/index.js";
+import { Op } from "sequelize";
 
 export async function createTask(req, res) {
     try {
@@ -27,14 +28,95 @@ export async function createTask(req, res) {
 
 export async function getTasks(req, res) {
     try {
-        const tasks = await Task.findAll(
-            {
-                where:{
-                    userId:req.user.userId
-                }
-            }
-        );
 
+        const where = {
+            userId: req.user.userId
+        };
+
+        const allowedPriorities = ['low', 'medium', 'high'];
+
+        if (req.query.priority) {
+
+            if (!allowedPriorities.includes(req.query.priority)) {
+                return res.status(400).json({
+                    message: "invalid priority"
+                });
+            }
+
+            where.priority = req.query.priority
+        }
+
+        const allowedStatueses = ["pending", "completed"];
+
+        if (req.query.status) {
+
+            if (!allowedStatueses.includes(req.query.status)) {
+                return res.status(400).json({
+                    message: "invalid status"
+                });
+            }
+
+            where.status = req.query.status;
+        }
+        if (req.query.dueDateFrom || req.query.dueDateTo) {
+
+            if (req.query.dueDateFrom && isNaN(Date.parse(req.query.dueDateFrom))) {
+                return res.status(400).json({
+                    message: 'invalid duedatefrom'
+                });
+            }
+            if (req.query.dueDateTo && isNaN(Date.parse(req.query.dueDateTo))) {
+                return res.status(400).json({
+                    message: "invalid duedateto"
+                });
+            }
+
+            where.dueDate = {};
+
+            if (req.query.dueDateFrom) {
+                where.dueDate[Op.gte] = req.query.dueDateFrom;
+            }
+            if (req.query.dueDateTo) {
+                where.dueDate[Op.lte] = req.query.dueDateTo;
+            }
+
+        }
+        const allowedSorts = ["dueDate", "priority"];
+
+        let sortBy;
+        let order;
+
+        if (req.query.sortBy) {
+
+            if (!allowedSorts.includes(req.query.sortBy)) {
+                return res.status(400).json({
+                    message: "invalid sorting"
+                });
+            }
+            sortBy = req.query.sortBy;
+
+            if (req.query.order) {
+
+                const requestedOrder = req.query.order.toUpperCase();
+
+                if (requestedOrder !== "ASC" && requestedOrder !== "DESC") {
+                    return res.status(400).json({
+                        message: "invalid order"
+                    });
+                }
+                order = requestedOrder;
+            }
+        }
+        
+        const options = {
+            where
+        };
+
+        if(sortBy){
+            options.order =[sortBy,order ||"ASC"];
+        }
+
+        const tasks = await Task.findAll({options});
         res.status(200).json(tasks);
 
     } catch (error) {
@@ -47,84 +129,84 @@ export async function getTasks(req, res) {
     }
 }
 
-export async function getTask(req,res) {
-    try{
+export async function getTask(req, res) {
+    try {
         const task = await Task.findOne({
-            where:{
-                id:req.params.id,
-                userId:req.user.userId
+            where: {
+                id: req.params.id,
+                userId: req.user.userId
             }
         });
-        if(!task){
+        if (!task) {
             return res.status(404).json({
-                message:"task not found"
+                message: "task not found"
             });
         }
         res.status(200).json(task);
     }
-    catch(error){
+    catch (error) {
         console.error(error);
         res.status(500).json({
-            message:'failed to fetch task',
-            error:error.message
+            message: 'failed to fetch task',
+            error: error.message
         });
     }
-    
+
 }
 
-export async function updateTask(req,res) {
-    try{
+export async function updateTask(req, res) {
+    try {
         const task = await Task.findOne({
-            where:{
-                id:req.params.id,
-                userId:req.user.userId
+            where: {
+                id: req.params.id,
+                userId: req.user.userId
             }
         });
-        if(!task){
+        if (!task) {
             res.status(404).json({
-                message:"not found"
+                message: "not found"
             });
         }
         await task.update({
-            title:req.body.title,
-            description:req.body.description,
-            priority:req.body.priority,
-            dueDate:req.body.dueDate,
-            status:req.body.status
+            title: req.body.title,
+            description: req.body.description,
+            priority: req.body.priority,
+            dueDate: req.body.dueDate,
+            status: req.body.status
         });
         res.status(200).json(task);
     }
-    catch(error){
+    catch (error) {
         res.status(500).json({
-            message:"failed to update task",
-            error:error.message
+            message: "failed to update task",
+            error: error.message
         });
     }
 
 }
 
-export async function deleteTask(req,res) {
-    try{
+export async function deleteTask(req, res) {
+    try {
         const task = await Task.findOne({
-            where:{
-                id:req.params.id,
-                userId:req.user.userId
+            where: {
+                id: req.params.id,
+                userId: req.user.userId
             }
         });
-        if(!task){
+        if (!task) {
             res.status(404).json({
-                message:"not found"
+                message: "not found"
             });
         }
         await task.destroy();
         res.status(200).json({
-            message:"task deleted sucessfully"
+            message: "task deleted sucessfully"
         });
     }
-    catch(error){
+    catch (error) {
         res.status(500).json({
-            message:'failed to fetch',
-            error:error.message
+            message: 'failed to fetch',
+            error: error.message
         });
     }
 }
